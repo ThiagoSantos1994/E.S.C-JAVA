fcdd25257ca39649f6ca8cf4c53fb77aeb9f9d7b
ThiagoSantos1994 <thi_santos123@hotmail.com>
2020-11-18 18:35:47 -0300

    JAVA - 18.11.2020 - Refatoracao Codigo e Versao POM.xml

--- workspace/ESC Projeto/ESC-ExcelenciaEmSistemasComerciais/src/main/java/br/esc/software/EscExcelenciaEmSistemasComerciaisApplication.java
+++ workspace/ESC Projeto/ESC-ExcelenciaEmSistemasComerciais/src/main/java/br/esc/software/EscExcelenciaEmSistemasComerciaisApplication.java
@@ -1,18 +1,14 @@
 package br.esc.software;
 
-import java.sql.SQLException;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import br.esc.software.commons.exceptions.ExcecaoGlobal;
 
 @SpringBootApplication
 public class EscExcelenciaEmSistemasComerciaisApplication {
 
-	/* Alterado para API em 29/06/2020 */
+    /* Alterado para API em 29/06/2020 */
 
-	public static void main(String[] args) throws ExcecaoGlobal, SQLException {
-		SpringApplication.run(EscExcelenciaEmSistemasComerciaisApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EscExcelenciaEmSistemasComerciaisApplication.class, args);
+    }
 }
