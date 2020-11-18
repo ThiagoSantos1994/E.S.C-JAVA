25940abf988a7e157b02f43780bfd06ca92da065
ThiagoSantos1994 <thi_santos123@hotmail.com>
2020-11-18 17:35:19 -0300

    JAVA - 18.11.2020 - Implementacao OAuth2 SecurityAPIs

---
+++ workspace/ESC Projeto/ESC-ExcelenciaEmSistemasComerciais/src/main/java/br/esc/software/configuration/SegurancaOAuth2Config.java
@@ -0,0 +1,37 @@
+package br.esc.software.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+/**
+ * Referencia de implementação: https://www.youtube.com/watch?v=UsM2BY20Ux4
+ */
+
+@EnableWebSecurity
+@EnableAuthorizationServer
+@EnableResourceServer
+public class SegurancaOAuth2Config extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("escvb6").password("13509").roles("ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
