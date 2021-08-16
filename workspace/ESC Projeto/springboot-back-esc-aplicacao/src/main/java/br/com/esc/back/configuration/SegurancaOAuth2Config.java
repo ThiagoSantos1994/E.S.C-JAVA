d0e05bc74aabb8ab4722f56b0222d519d9502c64
ThiagoSantos1994 <56056923+ThiagoSantos1994@users.noreply.github.com>
2021-08-16 12:35:31 -0300

    JAVA SPRING BACK - 16.08.21 - Implementacao OAuth2

---
+++ workspace/ESC Projeto/springboot-back-esc-aplicacao/src/main/java/br/com/esc/back/configuration/SegurancaOAuth2Config.java
@@ -0,0 +1,42 @@
+package br.com.esc.back.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+import static br.com.esc.back.commons.GlobalUtils.getProperties;
+
+
+/**
+ * Referencia de implementação: https://www.youtube.com/watch?v=UsM2BY20Ux4
+ */
+
+@EnableWebSecurity
+@EnableAuthorizationServer
+@EnableResourceServer
+public class SegurancaOAuth2Config extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("appfrontesc")
+                .password("13509")
+                .roles("ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
